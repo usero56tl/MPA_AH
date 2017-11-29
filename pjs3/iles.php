1b37c005f9904df4a2f2cf47e4ef1c7bce0fe7e2
Obalie <32571265+Obalie@users.noreply.github.com>
2017-11-29 11:43:10 +0100

    Update iles.php

--- pjs3/iles.php
+++ pjs3/iles.php
@@ -8,7 +8,7 @@
 					
 						<div class="article-ile">
 							<div class="container-ile">
-								<a href="https://fr.wikipedia.org/wiki/Marie-Galante">
+								<a href="https://fr.wikipedia.org/wiki/Marie-Galante" target="_blank">
 									<img src="img/marie-galante.jpg"  alt="Marie-Galante"/>
 								</a>
 							</div>
@@ -18,7 +18,7 @@
 								Situ&eacute;e &agrave; 30 km au sud est des c&ocirc;tes de la Guadeloupe et surnomm&eacute;e la Grande Galette &agrave; cause de sa forme ronde. Il s&apos;agit de la patrie de la canne &agrave; sucre et du rhum. En effet, de vaste champs de cannes &agrave; sucre occupent de nos jours la majorit&eacute; des terres
 									
 								<div>
-									<a href="https://fr.wikipedia.org/wiki/Marie-Galante ">
+									<a href="https://fr.wikipedia.org/wiki/Marie-Galante " target="_blank">
 										<input type="button" class="btn-ile" value="Plus ..."/>
 									</a>
 								</div>
@@ -28,7 +28,7 @@
 							  
 						<div class="article-ile">
 							<div class="container-ile">
-								<a href="https://fr.wikipedia.org/wiki/&Icirc;les_des_Saintes">
+								<a href="https://fr.wikipedia.org/wiki/&Icirc;les_des_Saintes" target="_blank">
 									<img src="img/les-saintes.jpg" alt="Les Saintes"/>
 								</a>					
 							</div>	
@@ -38,7 +38,7 @@
 								Baptis&eacute; &agrave; l&apos;origine Los Santos par Christophe Colomb ou encore Lesent en cr&eacute;ole.Archipel devenue fran&ccedil;aise le 18 octobre 1648 compos&eacute de 9 &icirc;lots. Un cadre paisible et idyllique	
 											
 								<div id="btn-ile">
-									<a href="https://fr.wikipedia.org/wiki/&Icirc;les_des_Saintes">
+									<a href="https://fr.wikipedia.org/wiki/&Icirc;les_des_Saintes" target="_blank">
 										<input type="button" class="btn-ile" value="Plus ..." />
 									</a>							
 								</div>						
@@ -47,7 +47,7 @@
 							  
 						<div class="article-ile">
 							<div class="container-ile">
-								<a href="https://fr.wikipedia.org/wiki/Grande-Terre_(Guadeloupe)">
+								<a href="https://fr.wikipedia.org/wiki/Grande-Terre_(Guadeloupe)" target="_blank" >
 									<img src="img/grande-terre.jpg" alt="Grande Terre"/>
 								</a>					
 							</div>	
@@ -57,7 +57,7 @@
 								La petite Bretagne des Antilles, la plus des deux ailes de la Guadeloupe aussi surnomm&eacute;e papillon.De hautes falaises, il s’agit d’une &icirc;le color&eacute;e qui fait r&ecirc;ver avec ses magnifiques plages	
 											
 								<div id="btn-ile">
-									<a href="https://fr.wikipedia.org/wiki/Grande-Terre_(Guadeloupe)">
+									<a href="https://fr.wikipedia.org/wiki/Grande-Terre_(Guadeloupe)"target="_blank">
 										<input type="button" class="btn-ile" value="Plus ..." />
 									</a>
 								</div>
@@ -66,7 +66,7 @@
 								  
 						<div class="article-ile">
 							<div class="container-ile">
-								<a href="https://fr.wikipedia.org/wiki/Basse-Terre">
+								<a href="https://fr.wikipedia.org/wiki/Basse-Terre" target="_blank">
 									<img src="img/basse-terre.jpg" alt="Basse Terre" />
 								</a>					
 							</div>	
@@ -76,7 +76,7 @@
 								Une des deux ailes que forment ce papillon qu&acute;est la Guadeloupe est la Basse-Terre reconnaissable par la magnifique silhouette de son Volcan, la Grande-Dame. Ce volcan s&acute;&eacute;l&egrave;ve à plus de 1467 mètres. Il s’agit d’une &icirc;le qui invite au voyage et &agrave; l&acute;aventure ! 
 										
 								<div id="btn-ile">
-									<a href="https://fr.wikipedia.org/wiki/Basse-Terre">
+									<a href="https://fr.wikipedia.org/wiki/Basse-Terre" target="_blank">
 										<input type="button" class="btn-ile" value="Plus ..." />
 									</a>
 								</div>
@@ -85,7 +85,7 @@
 							  
 						<div class="article-ile">
 							<div class="container-ile">
-								<a href="https://fr.wikipedia.org/wiki/La_D&eacute;sirade_(&icirc;le)">
+								<a href="https://fr.wikipedia.org/wiki/La_D&eacute;sirade_(&icirc;le)" target="_blank">
 									<img src="img/desirade.jpg"  alt="Désirade"/>
 								</a>
 
@@ -96,7 +96,7 @@
 								La premi&egrave;re terre découverte par Christophe Colomb lors de son voyage le 2 novembre 1493. Une &icirc;le remplie d&acute;histoire
 											
 								<div id="btn-ile">
-									<a href="https://fr.wikipedia.org/wiki/La_D&eacute;sirade_(&icirc;le)">
+									<a href="https://fr.wikipedia.org/wiki/La_D&eacute;sirade_(&icirc;le)" target="_blank">
 										<input type="button" class="btn-ile" value="Plus ..." />
 									</a>
 								</div>
