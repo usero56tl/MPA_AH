f2ef021152dbe3adc3ec22d2bd8f59204b705708
inagavel <32570097+inagavel@users.noreply.github.com>
2017-12-12 23:04:19 +0100

    Add files via upload

--- pjs3/evenements.php
+++ pjs3/evenements.php
@@ -1,43 +1,76 @@
 <!--menu 	-->
 <?php require "haut.tpl";?>
 		<main>
-			<a href="evenement_description.php"/> Description </a>
-			<p></p>
+			
+			<div class="Conteneur_event">
+				<div class="meteo">
 
-			<div class="d_event">
-				<img src="img/img1.jpg" class="imgevent" alt="event">
-				<div class="transi">
-					<h2>ok</h2>
-					<div class="details">
-						<h2>ouai blabla</h2>
+					<iframe src="https://www.meteoblue.com/fr/meteo/widget/three/guadeloupe_guadeloupe_3579144?geoloc=fixed&nocurrent=0&noforecast=0&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=image"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups"></iframe><div><!-- DO NOT REMOVE THIS LINK --><a href="https://www.meteoblue.com/fr/meteo/prevision/semaine/guadeloupe_guadeloupe_3579144?utm_source=weather_widget&utm_medium=linkus&utm_content=three&utm_campaign=Weather%2BWidget" target="_blank"></a></div>
+				</div>
+
+				<div class="d_event">
+					<div class="d2_event">
+						<img src="img/boat.jpg" class="imgevent" alt="event">
+						<div class="transi">
+							<h2>ok</h2>
+							<div class="details">
+								<h2>ouai blabla</h2>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+							</div>
+						</div>
+						<form action="evenement_description.php">
+							<input class="bt-form" type="submit" value="S'inscrire">
+						</form>
+					</div>
+					
+					<div class="d2_event">
+						<img src="img/festival.jpg" class="imgevent" alt="event">
+						<div class="transi">
+							<h2>ok</h2>
+							<div class="details">
+								<h2>ouai blabla</h2>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+							</div>
+						</div>
+						<form action="evenement_description.php">
+							<input class="bt-form" type="submit" value="S'inscrire">
+						</form>
+					</div>
+		
+					<div class="d2_event">
+						<img src="img/randonee.jpg" class="imgevent" alt="event">
+						<div class="transi">
+							<h2>ok</h2>
+							<div class="details">
+								<h2>ouai blabla</h2>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+								<p>aaaaaaaaaa</p>
+							</div>
+						</div>
+						<form action="evenement_description.php">
+							<input class="bt-form" type="submit" value="S'inscrire">
+						</form>
 					</div>
 				</div>
-	
-				<img src="img/img1.jpg" class="imgevent" alt="event">
-				<div class="transi">
-					<h2>ok</h2>
-					<div class="details">
-						<h2>ouai blabla</h2>
-					</div>
-				</div>
-	
-				<img src="img/img1.jpg" class="imgevent" alt="event">
-				<div class="transi">
-					<h2>ok</h2>
-					<div class="details">
-						<h2>ouai blabla</h2>
-					</div>
-				</div>
+
 			</div>
-
-			<div meteo>
-
-				<iframe src="https://www.meteoblue.com/en/weather/widget/three?geoloc=detect&nocurrent=0&noforecast=0&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=image"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups" style="width: 460px;height: 594px"></iframe>
-				<div><!-- DO NOT REMOVE THIS LINK -->
-					<a href="https://www.meteoblue.com/en/weather/forecast/week?utm_source=weather_widget&utm_medium=linkus&utm_content=three&utm_campaign=Weather%2BWidget" target="_blank">meteoblue</a>
-				</div>
-			</div>
-
 
 <!--fin menu 	-->
 	<?php require "bas.tpl";?>
