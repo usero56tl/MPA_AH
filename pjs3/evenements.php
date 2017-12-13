91df1c797d55afe3f23f8922d2374b627a046cef
inagavel <32570097+inagavel@users.noreply.github.com>
2017-12-13 16:15:10 +0100

    Add files via upload

--- pjs3/evenements.php
+++ pjs3/evenements.php
@@ -2,37 +2,39 @@
 <?php require "haut.tpl";?>
 		<main>
 			
-			<div class="Conteneur_event">
+			<div class="conteneur_event">
 				<div class="meteo">
 
-					<iframe src="https://www.meteoblue.com/fr/meteo/widget/three/guadeloupe_guadeloupe_3579144?geoloc=fixed&nocurrent=0&noforecast=0&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=image"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups"></iframe><div><!-- DO NOT REMOVE THIS LINK --><a href="https://www.meteoblue.com/fr/meteo/prevision/semaine/guadeloupe_guadeloupe_3579144?utm_source=weather_widget&utm_medium=linkus&utm_content=three&utm_campaign=Weather%2BWidget" target="_blank"></a></div>
+					<iframe src="https://www.meteoblue.com/fr/meteo/widget/three/guadeloupe_guadeloupe_3579144?geoloc=fixed&nocurrent=0&noforecast=0&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=image"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups" ></iframe><div><!-- DO NOT REMOVE THIS LINK --><a href="https://www.meteoblue.com/fr/meteo/prevision/semaine/guadeloupe_guadeloupe_3579144?utm_source=weather_widget&utm_medium=linkus&utm_content=three&utm_campaign=Weather%2BWidget" target="_blank"></a></div>
 				</div>
 
 				<div class="d_event">
 					<div class="d2_event">
 						<img src="img/boat.jpg" class="imgevent" alt="event">
 						<div class="transi">
-							<h2>ok</h2>
+							<h1>Croisière aux Antilles</h1>
+							<h5>
+								22 décembre - 30 déc. 2017 à 14:00 - 19:00 -04
+                                Pointe-à-Pitre, Pointe-à-Pitre, Guadeloupe
+                            </h5>
 							<div class="details">
-								<h2>ouai blabla</h2>
-								<p>aaaaaaaaaa</p>
-								<p>aaaaaaaaaa</p>
-								<p>aaaaaaaaaa</p>
-								<p>aaaaaaaaaa</p>
-								<p>aaaaaaaaaa</p>
-								<p>aaaaaaaaaa</p>
-								<p>aaaaaaaaaa</p>
+								<p>
+									Partez en croisière au départ de Pointe-à-Pitre pour huit jours de voyage paradisiaque, à la découverte de l’archipel Guadeloupéen.
+								</p>
 							</div>
 						</div>
-						<form action="evenement_description.php">
-							<input class="bt-form" type="submit" value="S'inscrire">
-						</form>
+						<div class="btn-event">
+							<form action="evenement_description.php">
+								<input class="bt-form" type="submit" value="S'inscrire">
+							</form>
+						</div>
+						
 					</div>
 					
 					<div class="d2_event">
 						<img src="img/festival.jpg" class="imgevent" alt="event">
 						<div class="transi">
-							<h2>ok</h2>
+							<h1>ok</h1>
 							<div class="details">
 								<h2>ouai blabla</h2>
 								<p>aaaaaaaaaa</p>
@@ -44,15 +46,14 @@
 								<p>aaaaaaaaaa</p>
 							</div>
 						</div>
-						<form action="evenement_description.php">
+						
 							<input class="bt-form" type="submit" value="S'inscrire">
-						</form>
 					</div>
 		
 					<div class="d2_event">
 						<img src="img/randonee.jpg" class="imgevent" alt="event">
 						<div class="transi">
-							<h2>ok</h2>
+							<h1>ok</h1>
 							<div class="details">
 								<h2>ouai blabla</h2>
 								<p>aaaaaaaaaa</p>
@@ -64,9 +65,7 @@
 								<p>aaaaaaaaaa</p>
 							</div>
 						</div>
-						<form action="evenement_description.php">
 							<input class="bt-form" type="submit" value="S'inscrire">
-						</form>
 					</div>
 				</div>
 
