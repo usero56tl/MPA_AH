db0d55158926d5e6f7bca8af648e0754f733f8cb
Nawendil <33688438+Nawendil@users.noreply.github.com>
2017-12-18 15:36:53 +0100

    Add files via upload

--- pjs3/evenement_description.php
+++ pjs3/evenement_description.php
@@ -7,42 +7,45 @@
 <!-- metre ini le contenu -->
 
 <div class="conteneur_desc">
-	<h5>déc. 22, 2017  |  Pointe-à-Pitre</h5>
+	<a class="return " href="javascript:history.go(-1)">< Retour</a>
+	<center> 
+		<h5 class="marge_bas">déc. 22, 2017  |  Pointe-à-Pitre</h5>
 
-	<h1>Croisière aux Antilles</h1>
+	<div class="titre"><h1 class="marge_bas">Croisière aux Antilles</h1></div>
 
-	<div id = "txt1">
+	<div class="marge_bas">
 		<p>Partez en croisière au départ de Pointe-à-Pitre pour huit jours de voyage paradisiaque, à la découverte de l’archipel Guadeloupéen.</p>
 	</div>
 
 	<form action="evenement_inscription.php">
-		<input class="bt-form" type="submit" value="S'inscrire">
+		<input class="bt-form marge_bas" type="submit" value="S'inscrire">
 	</form>
-
+	
+	
+	<div id="tof">
 	<img src="img/boat.jpg" alt="Slide 1" width="70%" height="50%">
-	<h3>Heure et lieu</h3>
-	<p>
+	</div>
+	
+	<h3 class = "marge_bas" >Heure et lieu</h3>
+	<p class="marge_bas">
 		22 décembre - 30 déc. 2017 à 14:00 - 19:00 -04
 		Pointe-à-Pitre, Pointe-à-Pitre, Guadeloupe
 	</p>
 
-	<h3>À propos de l'événement</h3>
-	<p>
-		Une Croisière de huit jours à bord du Costa Magica . Départ du port de Pointe-à-Pitre (Guadeloupe), visitez Scarborough (Tobago), Grenade (Antilles), Bridgetown (La Barbade), Castries (Antilles), Fort-de-France (Martinique) et débarquez au port de Pointe-à-Pitre (Guadeloupe).
+	<h3 class="marge_bas">À propos de l'événement</h3>
+	<p class="marge_bas">
+		Une Croisière de huit jours à bord du Costa Magica . Départ du port de Pointe-à-Pitre (Guadeloupe), visitez Scarborough (Tobago), 
+		Grenade (Antilles), Bridgetown (La Barbade), Castries (Antilles), Fort-de-France (Martinique) et débarquez au port de Pointe-à-Pitre (Guadeloupe).
 		Embarquez dans un véritable paradis ambulant avec pas moins de neuf étages, une gastronomie hors norme et des boutiques de renom.
 	</p>
 
-	<form action="evenement_inscription.php">
+	<form class="marge_bas" action="evenement_inscription.php">
 		<input class="bt-form" type="submit" value="S'inscrire">
 	</form>
 
-	<p>
-		<center> 
-					<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d61280.48340053117!2d-61.648894!3d16.270222!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sus!4v1512994820796" width="700" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-		</center>
+	<p class="marge_bas">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d61280.48340053117!2d-61.648894!3d16.270222!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sus!4v1512994820796" width="700" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </center>
-
-		
 </div>
 
 <?php require "bas.tpl";?>
