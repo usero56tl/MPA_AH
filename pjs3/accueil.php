fff2ba54e891422025117b7de0443ef408e29eb3
kryyshna <33688650+kryyshna@users.noreply.github.com>
2017-12-18 15:18:08 +0100

    Add files via upload

--- pjs3/accueil.php
+++ pjs3/accueil.php
@@ -10,10 +10,10 @@
 			  	<div class="container_slider">
 					<div id="slider">
 						<figure>
-							<img src="img/img1.jpg" alt>
-							<img src="img/img2.jpg" alt>
-							<img src="img/img3.jpg" alt>
-							<img src="img/img4.jpg" alt>
+							<img src="img/img1.jpg">
+							<img src="img/img2.jpg">
+							<img src="img/img3.jpg">
+							<img src="img/img4.jpg">
 						</figure>
 					</div>
 					<div class="centree titre ">
@@ -22,10 +22,19 @@
 							<h1> GUADELOUPE </h1>
 							<input class="centree2" type="text" name="nom" value="Recherche...">
 							<div class="btn-vers-bas">
-								<img src="img/vers_bas.png" alt id="monBouton">
+								<img src="img/vers_bas.png" alt id="monBoutonB">
 							</div>
 						</center>
 					</div>
 				</div>
+				<div class="info">
+					</br><h2 class="bold">A propos</h2></br>
+					<h2 class="apropos"> VisitAdept est une startup informatique en partenariat avec différentes agences de tourisme en </h2>
+					<h2 class="apropos"> Guadeloupe. Elle a été fondée en 2017 par un groupe d'étudiants soucieux de vous faire voyager. </h2>
+					<h2 class="apropos"> Vous trouverez ici les meilleurs conseils pour visiter l’île au Papillon. </h2></br>
+					
+					</br><h2 class="bold">Nous contacter</h2></br>
+					<a href="mailto:contact@visitadept.com"><h2 class="contact">contact@visitadept.com</h2></a>
+					<a href="https://fr-fr.facebook.com"><img class="part" src="img/fb.png"><img class="part" src="img/tw.jpg"></a></br></br>
 		<!--fin main 	-->
 <?php require "bas.tpl";?>
