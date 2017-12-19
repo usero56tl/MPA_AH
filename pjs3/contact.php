43c1f8f0a79c3fac3232c3766091b2da4bbc1c8f
usero56tl <32571033+usero56tl@users.noreply.github.com>
2017-12-19 08:32:58 +0100

    Update contact.php

--- pjs3/contact.php
+++ pjs3/contact.php
@@ -18,7 +18,7 @@
 				</div>
 
 				<div id="mapContact">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d490544.3819083328!2d-61.677251632626295!3d16.149446596904557!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c0e755a77287d51%3A0xecbe3e37b74a07ab!2sGuadeloupe!5e0!3m2!1sfr!2sus!4v1513605605021" width=75% height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d490544.3819083328!2d-61.677251632626295!3d16.149446596904557!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c0e755a77287d51%3A0xecbe3e37b74a07ab!2sGuadeloupe!5e0!3m2!1sfr!2sus!4v1513605605021" width=90% height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 				</div>
 			</div>
 		</center>
