b32a8ca4e631f8fa0ebafcd1ef8cf9eefa621c79
Obalie <32571265+Obalie@users.noreply.github.com>
2018-01-02 20:07:05 +0100

    Add files via upload

--- pjs3/iles_grid.php
+++ pjs3/iles_grid.php
@@ -1,99 +1,120 @@
-<!--menu 	-->
-<?php require "haut.tpl";?>
-<!--fin menu 	-->
-<!DOCTYPE html>
-<html lang="fr">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html;charset=utf8" />
-		<title>Les &icirc;les </title>
-		<link rel="stylesheet" href="css/grid.css">	
-		<script src="script.js"></script>
-	</head>
-		
+	<!--menu 	-->
+	<?php require "haut.tpl";?>
 	
-		<div class="body-ile" >
-			<body>
-				
-				  <div class="titre"> <h1> Les diff&eacute;rentes &icirc;les de la Guadeloupe </h1> </div>
-				  
-				  <div class="wrapper">
-					<div class="container-ile">
-						<a href="https://fr.wikipedia.org/wiki/Marie-Galante"  target="_blank" ><img src="img/marie-galante.jpg"  target="_blank" alt="Marie-Galante" style="vertical-align:text-top"/> </a>
-					</div>
+<!--	<!DOCTYPE html>-->
+		<main>
+<!--	<html lang="fr">
+		<head>
+			<meta http-equiv="Content-Type" content="text/html;charset=utf8" />
+			<title>Les &icirc;les </title>
+			<link rel="stylesheet" href="css/grid.css">	
+			<script src="script.js"></script>
+		</head>
+			-->
+			<div class="body-ile" >
 
-					<div class="description-ile">		
-						<h3> Marie-Galante </h3>	
-						Situ&eacute;e &agrave; 30 km au sud est des c&ocirc;tes de la Guadeloupe et surnomm&eacute;e la Grande Galette
-						&agrave; cause de sa forme ronde. Il s&apos;agit de la patrie de la canne &agrave; sucre et du rhum.
-						En effet, de vaste champs de cannes &agrave; sucre occupent de nos jours la majorit&eacute; des terres			
-						<div id="btn-ile">
-							<a href="https://fr.wikipedia.org/wiki/Marie-Galante "  target="_blank"> <input type="button" value="Plus ..." /> </a>
-						</div>
-					</div>	
-				  </div>
-				  
-				  
-				  
-				   <div class="wrapper">
+					<div class="titre">
+						<h1> Les diff&eacute;rentes &icirc;les de la Guadeloupe </h1>
+					</div>
+					  
+					  <div class="wrapper">
 						<div class="container-ile">
-							<a href="https://fr.wikipedia.org/wiki/&Icirc;les_des_Saintes"  target="_blank"><img src="img/les-saintes.jpg"  target="_blank" alt="Les Saintes" style="vertical-align:text-top" /></a>					
-						</div>	
-				
-						<div class="description-ile">
-							<h3> Les Saintes </h3>					
-								Baptis&eacute; &agrave; l&apos;origine Los Santos par Christophe Colomb ou encore Lesent en cr&eacute;ole. 
-								Archipel devenue fran&ccedil;aise le 18 octobre 1648 compos&eacute de 9 &icirc;lots. Un cadre paisible et idyllique	
-								
-								<div id="btn-ile">
-									<a href="https://fr.wikipedia.org/wiki/&Icirc;les_des_Saintes"  target="_blank"><input type="button" value="Plus ..." /></a>							
-								</div>						
+							<a href="https://fr.wikipedia.org/wiki/Marie-Galante" >
+								<img src="img/marie-galante.jpg" alt="Marie-Galante"/> 
+							</a>
 						</div>
-				  </div>
-				  
-				   <div class="wrapper">
-						<div class="container-ile">
-							<a href="https://fr.wikipedia.org/wiki/Grande-Terre_(Guadeloupe)"  target="_blank"> <img src="img/grande-terre.jpg"  target="_blank" alt="Grande Terre" style="vertical-align:text-top" /></a>					
+
+						<div class="description-ile">		
+							<h3> Marie-Galante </h3>	
+							Situ&eacute;e &agrave; 30 km au sud est des c&ocirc;tes de la Guadeloupe et surnomm&eacute;e la Grande Galette
+							&agrave; cause de sa forme ronde. Il s&apos;agit de la patrie de la canne &agrave; sucre et du rhum.
+							En effet, de vaste champs de cannes &agrave; sucre occupent de nos jours la majorit&eacute; des terres			
+							
+							<div>
+									<form action="https://fr.wikipedia.org/wiki/Marie-Galante">
+										<input type="submit" class="btn-ile" value="Plus ..."/>
+									</form>					
+							</div>
 						</div>	
-		  
-						<div class="description-ile">
-							<h3> Grande-Terre </h3>					
-								La petite Bretagne des Antilles, la plus des deux ailes de la Guadeloupe aussi surnomm&eacute;e papillon. 
-								De hautes falaises, il s’agit d’une &icirc;le color&eacute;e qui fait r&ecirc;ver avec ses magnifiques plages	
-								
-								<div id="btn-ile">
-									<a href="https://fr.wikipedia.org/wiki/Grande-Terre_(Guadeloupe)"  target="_blank"><input type="button" value="Plus ..." /></a>
-								</div>
-						</div>
 					  </div>
 					  
-				   <div class="wrapper">
-						<div class="container-ile">
-							<a href="https://fr.wikipedia.org/wiki/Basse-Terre" target="_blank" ><img src="img/basse-terre.jpg"  target="_blank" alt="Basse Terre" style="vertical-align:text-top"/></a>					
-						</div>	
-						
-						<div class="description-ile">
-							<h3> Basse-Terre </h3>					
-							Une des deux ailes que forment ce papillon qu&acute;est la Guadeloupe est la Basse-Terre reconnaissable par la magnifique silhouette de son Volcan, 
-							la Grande-Dame.
-							Ce volcan s&acute;&eacute;l&egrave;ve &agrave; plus de 1467 m&egrave;tres. Il s’agit d’une &icirc;le qui invite au voyage et &agrave; l&acute;aventure ! 							
-							<div id="btn-ile">
-								<a href="https://fr.wikipedia.org/wiki/Basse-Terre"  target="_blank"><input type="button" value="Plus ..." /></a>
+					   <div class="wrapper">
+							<div class="container-ile">
+								<a href="https://fr.wikipedia.org/wiki/&Icirc;les_des_Saintes" >
+									<img src="img/les-saintes.jpg" alt="Les Saintes" />
+								</a>					
+							</div>	
+					
+							<div class="description-ile">
+								<h3> Les Saintes </h3>					
+									Baptis&eacute; &agrave; l&apos;origine Los Santos par Christophe Colomb ou encore Lesent en cr&eacute;ole. 
+									Archipel devenue fran&ccedil;aise le 18 octobre 1648 compos&eacute; de 9 &icirc;lots. Un cadre paisible et idyllique	
+									
+									<div>
+											<form action="https://fr.wikipedia.org/wiki/&Icirc;les_des_Saintes">
+												<input type="submit" class="btn-ile" value="Plus ..." />
+											</form>			
+									</div>						
 							</div>
-						</div>
-				  </div>
-				  
-				   <div class="wrapper">
-						<div class="container-ile"><a href="https://fr.wikipedia.org/wiki/La_D&eacute;sirade_(&icirc;le)"  target="_blank"><img src="img/desirade.jpg"  target="_blank" alt="Désirade"style="vertical-align:text-top" /></a></div>	
-						
-						<div class="description-ile">
-							<h3> La d&eacute;sirade </h3>					
-								La premi&egrave;re terre découverte par Christophe Colomb lors de son voyage le 2 novembre 1493. Une &icirc;le remplie d&acute;histoire
-								<div id="btn-ile"><a href="https://fr.wikipedia.org/wiki/La_D&eacute;sirade_(&icirc;le)"  target="_blank"><input type="button" value="Plus ..." /></a></div>
-						</div>
-				  </div>
-				
-			</body>
-		</div>	
-	</html>
+					  </div>
+					  
+					   <div class="wrapper">
+							<div class="container-ile">
+								<a href="https://fr.wikipedia.org/wiki/Grande-Terre_(Guadeloupe)">
+								<img src="img/grande-terre.jpg"  alt="Grande Terre" /></a>					
+							</div>	
+			  
+							<div class="description-ile">
+								<h3> Grande-Terre </h3>					
+									La petite Bretagne des Antilles, la plus des deux ailes de la Guadeloupe aussi surnomm&eacute;e papillon. 
+									De hautes falaises, il s’agit d’une &icirc;le color&eacute;e qui fait r&ecirc;ver avec ses magnifiques plages	
+									
+									<div>
+										<form action="https://fr.wikipedia.org/wiki/Grande-Terre_(Guadeloupe)">
+											<input type="submit" class="btn-ile" value="Plus ..." />
+										</form>	
+									</div>
+							</div>
+						  </div>
+						  
+					   <div class="wrapper">
+							<div class="container-ile">
+								<a href="https://fr.wikipedia.org/wiki/Basse-Terre" >
+								<img src="img/basse-terre.jpg"  alt="Basse Terre"/></a>					
+							</div>	
+							
+							<div class="description-ile">
+								<h3> Basse-Terre </h3>					
+								Une des deux ailes que forment ce papillon qu&acute;est la Guadeloupe est la Basse-Terre reconnaissable par la magnifique silhouette de son Volcan, 
+								la Grande-Dame.
+								Ce volcan s&acute;&eacute;l&egrave;ve &agrave; plus de 1467 m&egrave;tres. Il s’agit d’une &icirc;le qui invite au voyage et &agrave; l&acute;aventure ! 							
+								<div>
+									<form action="https://fr.wikipedia.org/wiki/Basse-Terre">
+										<input type="submit" class="btn-ile" value="Plus ..." />
+									</form>	
+								</div>
+							</div>
+					  </div>
+					  
+					   <div class="wrapper">
+							<div class="container-ile">
+								<a href="https://fr.wikipedia.org/wiki/La_D&eacute;sirade_(&icirc;le)">
+									<img src="img/desirade.jpg" alt="Désirade"/>
+								</a>
+							</div>	
+							
+							<div class="description-ile">
+								<h3> La d&eacute;sirade </h3>					
+									La premi&egrave;re terre découverte par Christophe Colomb lors de son voyage le 2 novembre 1493. Une &icirc;le remplie d&acute;histoire
+									<div>
+										<form action="https://fr.wikipedia.org/wiki/La_D&eacute;sirade_(&icirc;le)">
+											<input type="submit"  class="btn-ile" value="Plus ..." />
+										</form>
+									</div>
+							</div>
+					  </div>
 
-	<?php require "bas.tpl";?>	
+			</div>	
+	<!--	</html>-->
+	<!-- bas de page -->
+		<?php require "bas.tpl";?>	
